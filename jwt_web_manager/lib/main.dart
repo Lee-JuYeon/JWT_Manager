88bd762c9866bffb03e24cccc0be3cd3433b02c4
cavss <cavss1118@gmail.com>
2024-01-10 19:17:21 +0900

    update webpage title

--- jwt_web_manager/lib/main.dart
+++ jwt_web_manager/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'JWT Web Manager',
       theme: ThemeData(
         // This is the theme of your application.
         //
